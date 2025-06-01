5cc45735d3691e5f7414e700d27e36076f64d3f1
speshiou <speshiou@gmail.com>
2025-06-01 13:59:22 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "prod-files-secure.s3.**.amazonaws.com",
+        hostname: "**.amazonaws.com",
       },
     ],
     deviceSizes: [320, 640, 800, 1200],
