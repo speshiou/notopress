fadacd90c90714d8062dd25421e333af18eee0d9
speshiou <speshiou@gmail.com>
2025-06-01 14:06:37 +0800

    Revert "Update next.config.ts"
    This reverts commit 5cc45735d3691e5f7414e700d27e36076f64d3f1.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.amazonaws.com",
+        hostname: "prod-files-secure.s3.**.amazonaws.com",
       },
     ],
     deviceSizes: [320, 640, 800, 1200],
